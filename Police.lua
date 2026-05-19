448700b2ee4166150b2eb8756bc0114153f23619
KERRYGAMER <148004830+lilkerry@users.noreply.github.com>
2026-05-18 20:29:44 -0400

    Add robbery configuration settings

---
+++ Police.lua
@@ -0,0 +1,8 @@
+Config.Robbery = {
+    minPolice = 2,        -- Minimum police on duty
+    maxPolice = 10,       -- Maximum police that can respond
+    minCrew = 2,          -- Minimum crew members
+    maxCrew = 4,          -- Maximum crew members
+    cooldown = 30 * 60,   -- Cooldown in seconds (30 mins)
+    reward_per_player = 5000,  -- Base reward per player
+}
