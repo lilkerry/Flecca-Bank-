009efe3030485a6a803b762ebac3379d547cf913
KERRYGAMER <148004830+lilkerry@users.noreply.github.com>
2026-05-18 20:28:58 -0400

    Add commands for bank robbery functionality

---
+++ Player.lua
@@ -0,0 +1,6 @@
+/bankrobbery [bank_id]    - Start a bank robbery (1-3)
+/bankrobberytest [id]     - Teleport to bank for testing
+/hacksafe                 - Hack the safe (requires hacking skill)
+/drillsafe                - Drill the safe (takes longer)
+/collectloot              - Collect money and items from safe
+/cancelrobbery            - Cancel the robbery
