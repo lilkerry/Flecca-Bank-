f44941391c918e1b5f9376fdb807dc2a741ec6e0
KERRYGAMER <148004830+lilkerry@users.noreply.github.com>
2026-05-18 20:23:21 -0400

    Add fxmanifest for Flecca Bank Robbery System

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KERRYGAMER'
+description 'Realistic Flecca Bank Robbery System - QBCore/QBX'
+version '1.0.0'
+
+shared_scripts {
+    'shared/config.lua',
+}
+
+client_scripts {
+    'client/client.lua',
+    'client/effects.lua',
+    'client/utils.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+    'server/logs.lua',
+}
+
+lua54 'yes'
