1caf437436e37d693b9c1b6ff17ff40220f441a0
KERRYGAMER <148004830+lilkerry@users.noreply.github.com>
2026-05-18 20:31:23 -0400

    Add loot configuration for money bag item

---
+++ Table.lua
@@ -0,0 +1,9 @@
+Config.Loot = {
+    {
+        item = 'money_bag',
+        label = 'Money Bag',
+        amount = {min = 1000, max = 2500},
+        weight = 1000,
+    },
+    -- Add more items here
+}
